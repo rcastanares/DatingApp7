b763c0b6a69fc66913817338fe975708fffd0e87
rcastanares <rcastanares@yondu.com>
2023-07-05 15:06:51 +0800

    End of Section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,11 +1,13 @@
-using System;
-using System.Security.Cryptography.X509Certificates;
-
 namespace API.Entities
 {
     public class AppUser
     {
         public int Id { get; set; }
+
         public string UserName { get; set; }
+        
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
     }
 }
